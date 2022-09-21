9a9c675b276a01aac893b85363f7d8dc6c0b59e0
freyj7 <freyj7@bfh.ch>
2022-09-21 20:38:55 +0200

    2

---
+++ module2.module.js
@@ -0,0 +1,10 @@
+var f_log_urls = function(s_import_meta_url){
+
+    console.log("s_import_meta_url")
+    console.log(s_import_meta_url)
+
+    console.log("import.meta.url")
+    console.log(import.meta.url)
+
+}
+export {f_log_urls}
